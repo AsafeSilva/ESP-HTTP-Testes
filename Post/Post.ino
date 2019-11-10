ea28d9d6ae00ee4d12ed9a5280109ee9c0fe9a7f
Asafe Silva <asafesilva01@gmail.com>
2019-11-10 00:02:15 -0300

    http post

---
+++ Post/Post.ino
@@ -0,0 +1,75 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* WIFI_NAME = "UN-MARCOS";
+const char* WIFI_PASSWORD = "1326081406";
+
+void setup(){
+	Serial.begin(115200);
+
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+
+	while(WiFi.status() != WL_CONNECTED){
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println();
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+	Serial.println();
+}
+
+void loop(){
+
+	if (WiFi.status() == WL_CONNECTED){
+		
+		HTTPClient client;
+
+		client.begin("http://jsonplaceholder.typicode.com/todos");
+		client.addHeader("Content-Type", "application/json");
+
+		StaticJsonDocument<100> jsonDoc;
+
+		jsonDoc["userId"] = 2;
+		jsonDoc["title"] = "Hello World!";
+		jsonDoc["completed"] = "true";
+
+		char jsonMessage[100];
+		serializeJsonPretty(jsonDoc, jsonMessage, sizeof(jsonMessage));
+		Serial.println(jsonMessage);
+		Serial.println();
+
+		int httpCode = client.POST(jsonMessage);
+
+		if (httpCode > 0){
+
+			String res = client.getString();
+
+			Serial.print("HTTP CODE: ");
+			Serial.println(httpCode);
+			Serial.println("HTTP response: ");
+			Serial.println(res);
+			Serial.println();
+
+		}else{
+
+			Serial.println("HTTP POST falhou!");
+			Serial.println("Erro: ");
+			Serial.println(client.errorToString(httpCode));
+		
+		}
+
+		client.end();
+
+	}else{
+
+		Serial.println("Erro na conexão!");
+
+	}
+
+	delay(30000);
+
+}
